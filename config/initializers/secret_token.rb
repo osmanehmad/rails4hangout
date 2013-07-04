824516379a2c187836a6f0e46afa4a781bbc3b74
Usman Ahmed <osman.ehmad@gmail.com>
2013-07-04 23:38:00 +0500

    mongoid working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveStreaming::Application.config.secret_key_base = '32316e0ef25e96c09769a5116607b0a9f8a1590516bfd92da51ad358d95b10ad51373a7a0abbc19f89641df05d157b3739e6aec63af7d3adc8c25b267a8ea9b6'
