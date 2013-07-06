f846a3e8f39d5a751e328b0cb0736a5b72de6dcc
Usman Ahmed <osman.ehmad@gmail.com>
2013-07-06 17:21:53 +0500

    all set for streaming

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 LiveStreaming::Application.routes.draw do
   match "/live" => "streaming#live", :via => :get #rails 4 likes to explicitly know if a match is via get or post
   match "/ajax" => "ajax#index", :via => :get
-  match "/update" => "strong#update", :via => :get
 end
